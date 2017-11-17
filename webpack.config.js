be2f6219816f05c6228bd65d05315fb4580e15a9
ztian <zhu.weitian@foxmail.com>
2017-11-17 22:06:39 +0800

    online zmall v1.4 ipaddress confgire

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 * @Author: Rosen
 * @Date:   2016-11-20 13:19:28
 * @Last Modified by:   ztian
-* @Last Modified time: 2017-11-17 21:32:54
+* @Last Modified time: 2017-11-17 22:00:14
 * 知识点：css单独打包、全局jquery引用、各种loader
 */
 
@@ -26,7 +26,7 @@ var config = {
     // path && publickPath  //s.zwtzmall.cn/admin-fe/dist/
     output: {
         path        : __dirname + '/dist/',
-        publicPath  : WEBPACK_ENV === 'dev' ? '/dist/' : '//111.230.231.35/admin-fe/dist/',
+        publicPath  : WEBPACK_ENV === 'dev' ? '/dist/' : '//111.230.231.35/frontend/admin-fe/dist/',
         filename    : 'js/[name].js'
     },
     resolve: {
